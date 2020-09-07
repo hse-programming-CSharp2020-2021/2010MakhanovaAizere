3ecfd2b4c216a2aa3f01220f85c91e12e6ee21df
Айзере Маханова <amakhanova@edu.hse.ru>
2020-09-07 21:46:30 +0600

    Задание для самостоятельного выполнени в классе

---
+++ 01module/1seminar/Homework1/Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c;
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+            c = Console.ReadLine();
+            Console.WriteLine(a+"!"+b+"!"+c);
+        }
+    }
+}
