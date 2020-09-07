221eb8a70c31e57deb378b67dc747db8f3b4d8d3
Айзере Маханова <amakhanova@edu.hse.ru>
2020-09-07 21:50:36 +0600

    Задание для самостоятельного выполнения в классе

---
+++ 01module/1seminar/Homework1/Task11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c;
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+            c = Console.ReadLine();
+            Console.WriteLine("-" + a + "-");
+            Console.WriteLine("-" + b + "-");
+            Console.WriteLine("-" + c + "-");
+        }
+    }
+}
